bfb195952e05d83364b41b07272ab9b85c15f24b
Trevor <tbliss@salesforce.com>
2019-03-15 14:34:40 -0700

    Add more paths to setup command

--- src/commands/lwc/test/setup.ts
+++ src/commands/lwc/test/setup.ts
@@ -1,4 +1,4 @@
-import { core, flags, SfdxCommand } from '@salesforce/command';
+import { core, SfdxCommand } from '@salesforce/command';
 import * as path from 'path';
 import * as fs from 'fs';
 import { spawnSync } from 'child_process';
@@ -16,11 +16,9 @@ export default class Run extends SfdxCommand {
 
   protected static requiresProject = true;
 
-  getProjectRoot(): string {
-    return process.cwd();
-  }
-
   public async run(): Promise<void> {
+    const project = await core.Project.resolve();
+
     const jestConfig = `const { jestConfig } = require('@salesforce/lwc-jest/config');
     module.exports = {
         ...jestConfig,
@@ -43,36 +41,9 @@ export default class Run extends SfdxCommand {
       throw new core.SfdxError(messages.getMessage('errorNpmNotFound'));
     }
 
-    // TODO(tbliss): add logic here to make sure we're in project root or find project root
-    const packageJsonPath = path.join(this.getProjectRoot(), 'package.json');
+    const packageJsonPath = path.join(project.getPath(), 'package.json');
     if (!fs.existsSync(packageJsonPath)) {
-      // we could run 'npm init' or create package.json ourselves
       throw new core.SfdxError(messages.getMessage('errorNoPackageJson'));
-    }
-
-    this.ux.log('Installing @salesforce/lwc-jest node package...');
-    //const lwcJestInstallRet = spawnSync('npm', ['add', '--save-dev', '@salesforce/lwc-jest'], { stdio: "inherit" });
-    // if (lwcJestInstallRet.error) {
-    //   throw new core.SfdxError(messages.getMessage('errorLwcJestInstall', [lwcJestInstallRet.error]));
-    // }
-
-    const forceignorePath = path.join(this.getProjectRoot(), '.forceignore');
-    if (!fs.existsSync(forceignorePath)) {
-      this.ux.log('Creating missing .forceignore file found in the project root...');
-      fs.writeFileSync(forceignorePath, forceignoreEntry);
-    } else {
-      const forceignore = fs.readFileSync(forceignorePath, { encoding: 'utf8' });
-      if (forceignore.indexOf('**/__tests__/**') === -1) {
-        this.ux.log('No "**/__tests__/** entry found in .forceignore. Adding now...');
-        fs.appendFileSync(forceignorePath, forceignoreEntry,{ encoding: 'utf8' });
-      }
-    }
-
-    // TODO(tbliss): should this also check for Jest config in package.json or other places?
-    const jestConfigPath = path.join(this.getProjectRoot(), 'jest.config.js');
-    if (!fs.existsSync(jestConfigPath)) {
-      this.ux.log('Creating jest.config.js configuration file in the project root...');
-      fs.writeFileSync(jestConfigPath, jestConfig);
     }
 
     const testScripts = {
@@ -85,6 +56,7 @@ export default class Run extends SfdxCommand {
     if (!scripts) {
       packageJson.scripts = testScripts;
     } else if (scripts["test:unit"] || scripts["test:unit:debug"] || scripts["test:unit:watch"]) {
+      // TODO(tbliss): error here or just log and ignore?
       throw new core.SfdxError(messages.getMessage('errorExistingScripts'));
     } else {
       packageJson.scripts = { ...scripts, ...testScripts};
@@ -92,6 +64,34 @@ export default class Run extends SfdxCommand {
     // TODO(tbliss): maybe make copy of package.json first to tmp location, replace if something goes wrong...
     fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 4), { encoding: 'utf8' });
 
+    this.ux.log('Installing @salesforce/lwc-jest node package...');
+    //const lwcJestInstallRet = spawnSync('npm', ['add', '--save-dev', '@salesforce/lwc-jest'], { stdio: "inherit" });
+    // if (lwcJestInstallRet.error) {
+    //   throw new core.SfdxError(messages.getMessage('errorLwcJestInstall', [lwcJestInstallRet.error]));
+    // }
 
+    const forceignorePath = path.join(project.getPath(), '.forceignore');
+    if (!fs.existsSync(forceignorePath)) {
+      this.ux.log('Creating missing .forceignore file found in the project root...');
+      fs.writeFileSync(forceignorePath, forceignoreEntry);
+    } else {
+      const forceignore = fs.readFileSync(forceignorePath, { encoding: 'utf8' });
+      if (forceignore.indexOf('**/__tests__/**') === -1) {
+        this.ux.log('No "**/__tests__/** entry found in .forceignore. Adding now...');
+        fs.appendFileSync(forceignorePath, forceignoreEntry,{ encoding: 'utf8' });
+      }
+    }
+
+    const jestConfigPath = path.join(project.getPath(), 'jest.config.js');
+    const packageJsonJest = packageJson.jest;
+    if (packageJsonJest) {
+      this.ux.log('Jest configuration found in package.json. Skipping creation of jest.config.js file.');
+    } else if (fs.existsSync(jestConfigPath)) {
+      this.ux.log('Jest configuration found in jest.config.js. Skipping creation of new config file.');
+    } else {
+      // no existing Jest config present in workspace
+      this.ux.log('Creating jest.config.js configuration file in the project root...');
+      fs.writeFileSync(jestConfigPath, jestConfig);
+    }
   }
 }
