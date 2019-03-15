2402fbb7c69628dcb5a2af0920b8117ab271a200
Trevor <tbliss@salesforce.com>
2019-03-15 00:20:00 -0700

    setup command package.json

--- src/commands/lwc/test/setup.ts
+++ src/commands/lwc/test/setup.ts
@@ -21,14 +21,12 @@ export default class Run extends SfdxCommand {
   }
 
   public async run(): Promise<void> {
-    /**
-     * - Check for Node version
-     * - Create package.json at project root if not available
-     * - @salesforce/lwc-jest added as devDependency
-     *  - verify what happens if already installed?
-     * - .forceignore updated
-     * ...
-     */
+    const jestConfig = `const { jestConfig } = require('@salesforce/lwc-jest/config');
+    module.exports = {
+        ...jestConfig,
+        // add any custom configurations here
+    };`;
+
     const forceignoreEntry = '# LWC Jest tests\n**/__tests__/**';
 
     const nodeVersionRet = spawnSync('node', ['-v']);
@@ -69,6 +67,31 @@ export default class Run extends SfdxCommand {
         fs.appendFileSync(forceignorePath, forceignoreEntry,{ encoding: 'utf8' });
       }
     }
+
+    // TODO(tbliss): should this also check for Jest config in package.json or other places?
+    const jestConfigPath = path.join(this.getProjectRoot(), 'jest.config.js');
+    if (!fs.existsSync(jestConfigPath)) {
+      this.ux.log('Creating jest.config.js configuration file in the project root...');
+      fs.writeFileSync(jestConfigPath, jestConfig);
+    }
+
+    const testScripts = {
+      "test:unit": "lwc-jest",
+      "test:unit:debug": "lwc-jest --debug",
+      "test:unit:watch": "lwc-jest --watch"
+    }
+    const packageJson = require(packageJsonPath);
+    const scripts = packageJson.scripts;
+    if (!scripts) {
+      packageJson.scripts = testScripts;
+    } else if (scripts["test:unit"] || scripts["test:unit:debug"] || scripts["test:unit:watch"]) {
+      throw new core.SfdxError(messages.getMessage('errorExistingScripts'));
+    } else {
+      packageJson.scripts = { ...scripts, ...testScripts};
+    }
+    // TODO(tbliss): maybe make copy of package.json first to tmp location, replace if something goes wrong...
+    fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 4), { encoding: 'utf8' });
+
 
   }
 }
