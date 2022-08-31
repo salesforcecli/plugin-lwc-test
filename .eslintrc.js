05baf9664d75804fe42320001e2be6abc5eeebd7
Nolan Lawson <nlawson@salesforce.com>
2022-08-31 10:34:43 -0700

    chore: update non-salesforce dependencies (@W-11466097@)

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,4 +6,9 @@
  */
 module.exports = {
   extends: ['eslint-config-salesforce-typescript', 'eslint-config-salesforce-license'],
+  rules: {
+    "@typescript-eslint/no-unsafe-argument": "off",
+    "@typescript-eslint/prefer-includes": "off",
+    "class-methods-use-this": "off"
+  }
 };
