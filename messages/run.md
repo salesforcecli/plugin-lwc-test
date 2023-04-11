5e032ab21bfaf2d71725b70ff783dbeb262dd4fe
Peter Hale <peter.hale@salesforce.com>
2023-04-11 06:25:02 -0600

    chore: wip

--- messages/run.md
+++ messages/run.md
@@ -28,6 +28,38 @@ Run tests when a watched file changes.
 
 Watched files include the component under test and any files it references.
 
+# flags.coverage.summary
+
+Collect coverage and display in output.
+
+# flags.coverage.description
+
+Collect coverage and display in output.
+
+# flags.updateSnapshot.summary
+
+Re-record every snapshot that fails during a test run.
+
+# flags.updateSnapshot.description
+
+Re-record every snapshot that fails during a test run.
+
+# flags.verbose.summary
+
+Display individual test results with the test suite hierarchy.
+
+# flags.verbose.description
+
+Display individual test results with the test suite hierarchy.
+
+# flags.skipApiVersionCheck.summary
+
+Disable the "sourceApiVersion" field check before running tests.
+
+# flags.skipApiVersionCheck.description
+
+**Warning** By disabling this check you risk running tests against stale versions of the framework. See details here: https://github.com/salesforce/sfdx-lwc-jest#disabling-the-sourceApiVersion-check
+
 # errorNoExecutableFound
 
 No sfdx-lwc-jest executable found. Verify it's properly installed.
--- src/commands/lightning/run/lwc/test.ts
+++ src/commands/lightning/run/lwc/test.ts
@@ -7,8 +7,8 @@
 import * as cp from 'child_process';
 import * as fs from 'fs';
 import * as path from 'path';
-import { Flags, loglevel, SfCommand } from '@salesforce/sf-plugins-core';
-import { Messages } from '@salesforce/core';
+import {Flags, loglevel, SfCommand} from '@salesforce/sf-plugins-core';
+import {Messages} from '@salesforce/core';
 
 Messages.importMessagesDirectory(__dirname);
 const messages = Messages.loadMessages('@salesforce/sfdx-plugin-lwc-test', 'run');
@@ -26,6 +26,7 @@ export default class RunTest extends SfCommand<RunResult> {
   public static readonly aliases = ['force:lightning:lwc:test:run'];
   public static strict = false;
   public static '--' = true;
+  // sfdx-lwc-jest flags `--version` and`--help` cannot be defined in flags given they are reserved by oclif
   public static readonly flags = {
     debug: Flags.boolean({
       char: 'd',
@@ -37,53 +38,86 @@ export default class RunTest extends SfCommand<RunResult> {
       description: messages.getMessage('flags.watch.description'),
       exclusive: ['debug'],
     }),
+    coverage: Flags.boolean({
+      summary: messages.getMessage('flags.coverage.summary'),
+      description: messages.getMessage('flags.coverage.description'),
+    }),
+    // eslint-disable-next-line sf-plugin/flag-case
+    updateSnapshot: Flags.boolean({
+      char: 'u',
+      summary: messages.getMessage('flags.updateSnapshot.summary'),
+      description: messages.getMessage('flags.updateSnapshot.description'),
+    }),
+    verbose: Flags.boolean({
+      summary: messages.getMessage('flags.verbose.summary'),
+      description: messages.getMessage('flags.verbose.description'),
+    }),
+    // eslint-disable-next-line sf-plugin/flag-case
+    skipApiVersionCheck: Flags.boolean({
+      summary: messages.getMessage('flags.skipApiVersionCheck.summary'),
+      description: messages.getMessage('flags.skipApiVersionCheck.description'),
+    }),
     loglevel,
   };
 
+  private flagKeys = Object.keys(RunTest.flags);
+
   public async run(): Promise<RunResult> {
     /* In order to ensure backwards compatibility with old SfdxCommand version of this command
-     * it was necessary to bypass the typical parsing of the command.
-     *
-     * This command defines two flags, debug and watch, which are probably the
-     * most used jest flags. The previous implementation of this command also
-     * defined an args configuration, (passthrough) which would allow any
-     * additional flags to be passed through to jest. This is no longer possible
-     * given how @oclif/core parses the command line, rejecting any flags that
-     * are not defined in the flag configuration.
-     *
-     * The solution is two-fold. First, we change the command to allow non-strict
-     * command configuration, as well as allowing the use of the pass through flag ('--').
-     *
-     * Second, we force the parse to use the config "{strict: false, '--': true}", instead
-     * of passing the command class to the parse method. This allows a parse of the command
-     * as if there are no flags defined, which results in the flags being parsed as arguments.
-     *
-     * Before calling the parse method, we filter out the '--' flag from the this.argv array and then
-     * call the parse method overriding the argv with the first element being '--' and the rest of the
-     * elements being the filtered argv array. This results in the parse method returning the flags
-     * as arguments.
-     *
-     * The resulting argv array is then passed to the runJest method as arguments.
+    * it was necessary to bypass the typical parsing of the command.
+    *
+    * This command defines two flags, debug and watch, which are probably the
+    * most used jest flags. The previous implementation of this command also
+    * defined an args configuration, (passthrough) which would allow any
+    * additional flags to be passed through to jest. This is no longer possible
+    * given how @oclif/core parses the command line, rejecting any flags that
+    * are not defined in the flag configuration.
+    *
+    * The solution is two-fold. First, we change the command to allow non-strict
+    * command configuration, as well as allowing the use of the pass through flag ('--').
+    *
+    * Second, we force the parse to use the config "{strict: false, '--': true}", instead
+    * of passing the command class to the parse method. This allows a parse of the command
+    * as if there are no flags defined, which results in the flags being parsed as arguments.
+    *
+    * Before calling the parse method, we filter out the '--' flag from the this.argv array and then
+    * call the parse method overriding the argv with the first element being '--' and the rest of the
+    * elements being the filtered argv array. This results in the parse method returning the flags
+    * as arguments.
+    *
+    * The resulting argv array is then passed to the runJest method as arguments.
      */
-
     // remove the '--', '--json' and 'loglevel' flags from the this.argv array
-    const tArgv = this.argv.filter((arg) => !['--', '--json', 'loglevel'].includes(arg));
+    const tArgv = this.argv.filter((arg) => !['--', '--json', '--loglevel'].includes(arg));
 
     const hasWatchFlag = tArgv.includes('--watch');
-    const hasDebugFlag = tArgv.some((arg) => /--debug|-d/.test(arg));
+    const hasDebugFlag = tArgv.some(arg => /--debug|-d/.test(arg));
 
     if (hasWatchFlag && hasDebugFlag) {
-      throw messages.createError('watchAndDebugAreMutuallyExclusive');
+      throw (messages.createError('watchAndDebugAreMutuallyExclusive'));
     }
 
     // call the parse method with alternate config and override the argv
     // with the first element being '--' and the rest of the elements being
-    const { argv } = await this.parse({ strict: false, '--': true }, ['--', ...tArgv]);
+    const {argv} = await this.parse({strict: false, '--': true},
+      ['--', ...tArgv]);
 
-    // add argv entries to the addArgs array
-    const addArgs = argv.map((arg) => arg as string);
+    // split into two arrays, one for command flags and one for remaining args
+    const cmdArgs = argv.map(arg => arg as string).reduce((parts, arg) => {
+      if (!arg.startsWith('-')) {
+        parts.right.push(arg);
+      }
+      const argSansHyphen = (arg).replace(/^-+/, '');
 
-    const scriptRet = this.runJest(addArgs);
+      if (this.flagKeys.some(key => key === argSansHyphen || RunTest.flags[key as keyof typeof RunTest.flags].char === argSansHyphen)) {
+        parts.left.push(arg);
+      } else {
+        parts.right.push(arg);
+      }
+      return parts;
+    }, {left: [] as string[], right: [] as string[]});
+
+    const scriptRet = this.runJest([...cmdArgs.left, '--',  ...cmdArgs.right]);
 
     const results: RunResult = {
       message: messages.getMessage('logSuccess', [scriptRet.status?.toString()]),
